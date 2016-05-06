f2d56e171eb703d3b2ce53cb25f9069d40667a85
Gene <red0@yandex.ru>
2016-05-05 21:49:42 -0700

    Add book and create migration

--- books/models.py
+++ books/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+	title = models.CharField(max_length = 255)
+	author = models.CharField(max_length = 255)
+	review = models.TextField(blank=True, null=True)
+	date_reviewd = models.DateTimeField(blank=True, null=True)
+	is_favorite = models.BooleanField(default=False)
