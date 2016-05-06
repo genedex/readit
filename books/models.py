b778420eb5c2d5756711c9a7703eb7712033e274
Gene <red0@yandex.ru>
2016-05-05 22:11:58 -0700

    Add supauser and register Book() in admin

--- books/models.py
+++ books/models.py
@@ -4,8 +4,11 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-	title = models.CharField(max_length = 255)
-	author = models.CharField(max_length = 255)
+	title = models.CharField(max_length = 255, verbose_name = "Book's title")
+	author = models.CharField(max_length = 25, help_text = "Use pen name, not real name!")
 	review = models.TextField(blank=True, null=True)
 	date_reviewd = models.DateTimeField(blank=True, null=True)
-	is_favorite = models.BooleanField(default=False)
+	is_favorite = models.BooleanField(default=False, verbose_name = "Favourite?")
+
+	def __str__(self):
+		return self.title
