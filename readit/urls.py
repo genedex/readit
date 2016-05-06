8ca03af8c7b2e5ef30abc14c0e83e35664bdee9f
Gene <red0@yandex.ru>
2016-05-05 23:32:15 -0700

    httpResponse

--- readit/urls.py
+++ readit/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
-
+from books.views import list_books
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'readit.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', list_books, name='books'),
 )
