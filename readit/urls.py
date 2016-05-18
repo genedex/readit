2de08cf63635c7693f47bbdd08be6165cfcb6dd1
Gene <red0@yandex.ru>
2016-05-18 00:13:41 -0700

    Views of books and authors

--- readit/urls.py
+++ readit/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
-from books.views import list_books
+from books.views import list_books, AuthorList, BookDetail, AuthorDetail
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'readit.views.home', name='home'),
@@ -10,4 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', list_books, name='books'),
+    url(r'^authors/$',AuthorList.as_view(), name = 'authors'),
+    url(r'^books/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name = 'book-detail'),
+    url(r'^authors/(?P<pk>[-\w]+)/$', AuthorDetail.as_view(), name = 'author-detail')
 )
